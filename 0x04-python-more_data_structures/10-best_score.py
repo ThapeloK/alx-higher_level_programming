44c0f3eaf1a87ab9a68f42ebef59b5198ebb5200
ThapeloK <e.thapelo@yahoo.com>
2020-12-17 20:58:19 +0000

    Fix issues

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def best_score(my_dict):
-    if my_dict:
-        return sorted(my_dict)[-1]
+def best_score(a_dictionary):
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
