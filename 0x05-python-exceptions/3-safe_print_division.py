321b92ae5dfa636365fadcdb1d886b53454b550d
ThapeloK <e.thapelo@yahoo.com>
2021-01-04 20:17:11 +0000

    Fix syntax error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = a / b
-    except:
-        result = None
+        results = a / b
+    except ZeroDivisionError:
+        results = None
     finally:
-        print("{}".format(result))
-    return (result)
+        print("Inside result: {}".format(results))
+    return results
