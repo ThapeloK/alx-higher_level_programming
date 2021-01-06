de3d3a520cde15658a72171c4894d3587e18af5f
ThapeloK <e.thapelo@yahoo.com>
2021-01-06 19:54:33 +0000

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,12 +6,7 @@ class Square:
     """Square class"""
     def __init__(self, size=0):
         """Constructor"""
-        #self.size = size
         self.size(size)
-
-    def area(self):
-        """Area of a square"""
-        return self.__size ** 2
 
     @property
     def size(self):
@@ -26,3 +21,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    def area(self):
+        """Area of a square"""
+        return self.__size ** 2
