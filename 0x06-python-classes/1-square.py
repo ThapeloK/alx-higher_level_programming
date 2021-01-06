1f132b4cdc6d39332d3126e57232ad3f93dc9768
ThapeloK <e.thapelo@yahoo.com>
2021-01-06 19:04:53 +0000

    Define square by base

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Simple class"""
+
+
+class Square:
+    """Square representation"""
+    def __init__(self, size):
+        """Constructor of Square class"""
+        self.__size = size
