186bd3dd579b229e8668c10240346311fea54959
ThapeloK <e.thapelo@yahoo.com>
2021-01-08 20:44:14 +0000

    Name printing function

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a name printing function"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints first and last name
+    Args: first_name, last_name
+    """
+    if type(first_name) != str:
+        raise TypeError('first_name must be a string')
+    if type(last_name) != str:
+        raise TypeError('last_name must be a string')
+    print("My name is {} {}".format(first_name, last_name))
