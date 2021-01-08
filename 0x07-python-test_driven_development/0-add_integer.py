b4ef53485e3d56003b39ef3ff6a4b45050f6121d
ThapeloK <e.thapelo@yahoo.com>
2021-01-08 20:28:06 +0000

    Added readme file and add integer task

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines an integer addition function"""
+
+
+def add_integer(a, b=98):
+    """a: integer or float
+    b: integer or float
+    Return: The sum of a and b"""
+
+    if a is None or (type(a) is not int and type(a) is not float):
+        raise TypeError("a must be an integer")
+    if type(b) != int and type(b) != float:
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
