c9004896e54ab70eb936c5341b754fb7dd9b31c5
ThapeloK <e.thapelo@yahoo.com>
2021-01-09 05:12:14 +0000

    Text indatation

---
+++ 0x07-python-test_driven_development/.#6-max_integer_test.py
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.4170:1609959117
---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a text indention function"""
+
+
+def text_indentation(text):
+    """Idents the given text"""
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    s = ""
+    for i in text:
+        if i in '.:?':
+            s += i
+            print(s.strip(" "), end="\n\n")
+            s = ""
+        else:
+            s += i
+    print(s.strip(" "), end="")
