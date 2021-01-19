e3a5945639899e48d7ebf2fbcedba5f4bb06c20b
ThapeloK <e.thapelo@yahoo.com>
2021-01-19 20:27:25 +0000

    Added write to a file and append to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Defines a function that gives the number of lines in a file."""
+
+
+def number_of_lines(filename=""):
+    """Return the number of lines in a text file."""
+    lines = 0
+    with open(filename) as f:
+        for line in f:
+            lines += 1
+    return lines
