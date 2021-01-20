a08d8c125ae2eabc0478437978b6c18779e5f5c7
ThapeloK <e.thapelo@yahoo.com>
2021-01-20 21:17:17 +0000

    Fix my task list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
 """Defines an inherited list class MyList."""
 
 
 class MyList(list):
-    """Implements sorted printing for the built-in list class."""
+    """Prints sorted version of list """
 
     def print_sorted(self):
-        """Print a list in sorted ascending order."""
         print(sorted(self))
