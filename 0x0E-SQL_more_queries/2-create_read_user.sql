6f20357071f6827e5b968fec2f51cbb47185729a
ThapeloK <e.thapelo@yahoo.com>
2021-03-03 19:44:16 +0000

    Added local project files to repo

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- Script that creates the database hbtn_0d_2 and the user user_0d_2
+-- Create a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Create a user
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- Give privileges to user_0d_2
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost'
