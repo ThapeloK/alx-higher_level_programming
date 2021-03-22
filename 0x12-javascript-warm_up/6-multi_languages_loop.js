aaa043ba6a3ddf1de4230db4bc28c450161bd6bc
ThapeloK <e.thapelo@yahoo.com>
2021-03-22 20:26:45 +0000

    Added multi language loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let i;
-for (i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(line);
 }
