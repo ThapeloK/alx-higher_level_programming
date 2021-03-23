5b754d2176a3b6d9ef9902a8c2bf25435c9a89a6
ThapeloK <e.thapelo@yahoo.com>
2021-03-23 19:08:08 +0000

    Imports and computes a dictionary of occurrences by user id

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const newdict = {};
+for (const key in dict) {
+  const val = dict[key];
+  if (newdict[val] === undefined) {
+    newdict[val] = [key];
+  } else {
+    newdict[val].push(key);
+  }
+}
+console.log(newdict);
